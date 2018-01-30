6b202a5f7582138c3f91ece3d8f3f4a34c362d1c
letrend <simon.trendel@tum.de>
2018-01-30 20:35:46 +0100

    with description

--- middleware/include/common_utilities/CommonDefinitions.h
+++ middleware/include/common_utilities/CommonDefinitions.h
@@ -1,9 +1,39 @@
-//
-// Created by bruh on 1/8/16.
-//
+/*
+    BSD 3-Clause License
 
-#ifndef ROBOY_CONTROL_COMMONDEFINITIONS_H
-#define ROBOY_CONTROL_COMMONDEFINITIONS_H
+    Copyright (c) 2017, Roboy
+            All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+            modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+    * Neither the name of the copyright holder nor the names of its
+    contributors may be used to endorse or promote products derived from
+    this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+            IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+            FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+            DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+            SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+            CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+    author: Simon Trendel ( simon.trendel@tum.de ), 2018
+    description: common defintions shared by all teams
+*/
+
+#pragma once
 
 #include <stdint.h>
 
@@ -142,5 +172,3 @@ typedef struct {
 	uint8_t mode;
 	uint8_t faults;
 }OOTXframe;
-
-#endif //ROBOY_CONTROL_COMMONDEFINITIONS_H
