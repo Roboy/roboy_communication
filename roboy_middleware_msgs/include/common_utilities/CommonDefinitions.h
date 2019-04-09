6f8c73aa8c47586be2f14ef63531dd436d0d3e54
Wagram Airiian <vagram.airiian@tum.de>
2019-04-09 23:07:25 +0200

    with all muscles

--- roboy_middleware_msgs/include/common_utilities/CommonDefinitions.h
+++ roboy_middleware_msgs/include/common_utilities/CommonDefinitions.h
@@ -182,7 +182,7 @@ typedef struct {
 #define LEG_LEFT 1
 #define LEG_RIGHT 2
 
-static std::map<int, std::vector<int>> active_motors = {{FPGA_LEFT, {0,1,2,3,4,5,6,7,8,15,16,17,18,19,20}},
+static std::map<int, std::vector<int>> active_motors = {{FPGA_LEFT, {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}},
                                                         {FPGA_RIGHT, {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}},
                                                         {UNKNOWN, {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}}};
 
